22400a981f28bca7739436cdc90f997199a2beb6
Zer Jun Eng <zjeng1@sheffield.ac.uk>
2016-12-01 18:00:30 +0000

    First implementation of colour board

--- floodit/floodit.rb
+++ floodit/floodit.rb
@@ -19,7 +19,7 @@ def main_menu(width, height, highscore)
   print "Please enter your choice: "
   mainInput = gets.chomp.downcase
   if mainInput == "s"
-    get_board(14, 9)
+    get_board(width, height)
   elsif mainInput == "c"
     print "Width (Currently #{width})? : "
     getWidth = gets.chomp.to_i
@@ -52,6 +52,34 @@ def get_board(width, height)
   # It is important that this method is used because
   # this will be used for checking the functionality
   # of your implementation.
+  board = Array.new(width) { Array.new(height) }
+  turns = 0;
+  colourArray = [:red, :blue, :green, :yellow, :cyan, :magneta]
+  
+  (0...height).each do |row|
+    (0...width).each do |column| 
+      board[row][column] = rand(0..5)
+      if (board[row][column] == 0)
+        print "  ".colorize(:background => :red)
+      elsif (board[row][column] == 1)
+        print "  ".colorize(:background => :blue)
+      elsif (board[row][column] == 2)
+        print "  ".colorize(:background => :green)
+      elsif (board[row][column] == 3)
+        print "  ".colorize(:background => :yellow)
+      elsif (board[row][column] == 4)
+        print "  ".colorize(:background => :cyan)
+      else (board[row][column] == 4)
+        print "  ".colorize(:background => :magenta)
+      end
+    end
+    puts
+  end
+  
+  puts "Number of turns: #{turns}"
+  puts "Current completetion: "
+  print "Choose a colour: "
+  colourInput = gets.chomp.to_s
 end
 
 # TODO: Implement everything else as described in the
@@ -70,4 +98,3 @@ loop do
   break if enter == "\n"
 end
 main_menu(14, 9, 0)
-
